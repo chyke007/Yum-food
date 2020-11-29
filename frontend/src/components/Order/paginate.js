994a6426b4b30a20a0f7e73fd1a2a75a3be50ca5
Nwachukwu Chibuike <nwachukwuchibuike2012@gmail.com>
2020-11-29 05:19:24 +0100

    Fixes pagination bug on mobile

--- frontend/src/components/Order/paginate.js
+++ frontend/src/components/Order/paginate.js
@@ -4,12 +4,13 @@ export default (props) => {
         return (
        <div className="bg-white w-10/12 overflow-auto px-4 py-3 flex items-center sm:px-6">
   <div className="flex-1 flex mt-12 justify-between sm:hidden">
-    <a href="/" className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
-      Previous
-    </a>
-    <a href="/" className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500">
-      Next
-    </a>
+
+  <button disabled={props.pagination && props.pagination.prev_page_url ? false : true} onClick={() => { props.nextPage(props.pagination.prev_page_url) }}  className={`relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 ${ !props.pagination.prev_page_url ? "cursor-not-allowed": "cursor-pointer"}`}>
+ Previous
+    </button>
+    <button disabled={props.pagination && props.pagination.next_page_url ? false : true} onClick={() => props.nextPage(props.pagination.next_page_url)} className={`relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 ${ !props.pagination.next_page_url ? "cursor-not-allowed": "cursor-pointer"}`}>
+          Next
+    </button>
   </div>
   <div className="hidden sm:flex-1 ml-32 sm:flex sm:items-center sm:justify-between my-16">
     {!props.loading && props.pagination && props.pagination.total > 0 && <div>
@@ -31,9 +32,9 @@ export default (props) => {
             <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
           </svg>
         </button>
-        <a href="/" className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
+        <span  className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50">
          {props.pagination && props.pagination.current_page} of {props.pagination && props.pagination.pages}
-        </a>
+        </span>
         <button disabled={props.pagination && props.pagination.next_page_url ? false : true} onClick={() => props.nextPage(props.pagination.next_page_url)} className={`relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 ${ !props.pagination.next_page_url ? "cursor-not-allowed": "cursor-pointer"}`}>
           <span className="sr-only">Next</span>
           <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
